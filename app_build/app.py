3f80eb3afcb48a13d56cd67941d946ba2e34b5d1
Cherie Wang <chereowang27@gmail.com>
2021-11-24 10:14:49 -0500

    fixed geolocation data upload code

---
+++ app_build/app.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import plotly.graph_objects as go
+import plotly.express as px
+
+import pandas as pd
